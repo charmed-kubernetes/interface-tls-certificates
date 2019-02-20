c7e2dd21ed6b68aadd5d23016ca4b6185362ce53
Cory Johns <johnsca@gmail.com>
2019-02-20 10:59:25 -0500

    Refactor provides side to versioned protocol

--- provides.py
+++ provides.py
@@ -1,8 +1,14 @@
+import weakref
+
 from charms.reactive import Endpoint
 from charms.reactive import when, when_not
 from charms.reactive import set_flag, clear_flag, toggle_flag
 
-from .tls_certificates_common import CertificateRequest
+from .tls_certificates_common import (
+    CertificateRequest,
+    Certificate,
+    VersionedProtocol,
+)
 
 
 class TlsProvides(Endpoint):
@@ -33,6 +39,12 @@ class TlsProvides(Endpoint):
     [new_server_requests]: provides.md#provides.TlsProvides.new_server_requests
     [new_client_requests]: provides.md#provides.TlsProvides.new_client_requests
     """
+    def _protocol(self, relation):
+        if not hasattr(self, '_protocols'):
+            ps = self._protocols = {}
+            for relation in self.relations:
+                ps[relation] = VersionedProtocol.negotiate(relation)
+        return self._protocols[relation]
 
     @when('endpoint.{endpoint_name}.joined')
     def joined(self):
@@ -61,16 +73,16 @@ class TlsProvides(Endpoint):
         Publish the CA to all related applications.
         """
         for relation in self.relations:
-            # All the clients get the same CA, so send it to them.
-            relation.to_publish_raw['ca'] = certificate_authority
+            protocol = self._protocol(relation)
+            protocol.set_root_ca_cert(certificate_authority)
 
     def set_chain(self, chain):
         """
         Publish the chain of trust to all related applications.
         """
         for relation in self.relations:
-            # All the clients get the same chain, so send it to them.
-            relation.to_publish_raw['chain'] = chain
+            protocol = self._protocol(relation)
+            protocol.set_root_ca_chain(chain)
 
     def set_client_cert(self, cert, key):
         """
@@ -79,10 +91,8 @@ class TlsProvides(Endpoint):
         Publish a globally shared client cert and key.
         """
         for relation in self.relations:
-            relation.to_publish_raw.update({
-                'client.cert': cert,
-                'client.key': key,
-            })
+            protocol = self._protocol(relation)
+            protocol.set_global_client_cert(cert, key)
 
     def set_server_cert(self, scope, cert, key):
         """
@@ -91,8 +101,11 @@ class TlsProvides(Endpoint):
 
         Set the server cert and key for the request identified by `scope`.
         """
-        request = self.get_server_requests()[scope]
-        request.set_cert(cert, key)
+        for relation in self.relations:
+            protocol = self._protocol(relation)
+            if scope in protocol.requests:
+                protocol.requests[scope].set_cert(cert, key)
+                break
 
     def set_server_multicerts(self, scope):
         """
@@ -115,7 +128,7 @@ class TlsProvides(Endpoint):
         Return a map of all server request objects indexed by a unique
         identifier.
         """
-        return {req._key: req for req in self.new_server_requests}
+        return {req.request_id: req for req in self.new_server_requests}
 
     @property
     def all_requests(self):
@@ -139,38 +152,9 @@ class TlsProvides(Endpoint):
         ```
         """
         requests = []
-        for unit in self.all_joined_units:
-            # handle older single server cert request
-            if unit.received_raw['common_name']:
-                requests.append(CertificateRequest(
-                    unit,
-                    'server',
-                    unit.received_raw['certificate_name'],
-                    unit.received_raw['common_name'],
-                    unit.received['sans'],
-                ))
-
-            # handle mutli server cert requests
-            reqs = unit.received['cert_requests'] or {}
-            for common_name, req in reqs.items():
-                requests.append(CertificateRequest(
-                    unit,
-                    'server',
-                    common_name,
-                    common_name,
-                    req['sans'],
-                ))
-
-            # handle client cert requests
-            reqs = unit.received['client_cert_requests'] or {}
-            for common_name, req in reqs.items():
-                requests.append(CertificateRequest(
-                    unit,
-                    'client',
-                    common_name,
-                    common_name,
-                    req['sans'],
-                ))
+        for relation in self.relations:
+            protocol = self._protocol(relation)
+            requests.extend(protocol.requests.values())
         return requests
 
     @property
@@ -249,4 +233,183 @@ class TlsProvides(Endpoint):
         List of all [Certificate][] instances that this provider has published
         for all related applications.
         """
-        return [req.cert for req in self.all_requests if req.cert]
+        certs = []
+        for relation in self.relations:
+            protocol = self._protocol(relation)
+            certs.extend(protocol.responses.values())
+        return certs
+
+
+class ProvidesV1(VersionedProtocol):
+    VERSION = 1
+
+    def upgrade_from(self, old_version):
+        pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.requests = {}
+        self.responses = {}
+        self._read_requests()
+        self._read_responses()
+
+    def _read_requests(self):
+        for unit in self.relation.joined_units:
+            # handle older single server cert request
+            common_name = unit.received_raw.get('common_name')
+            if common_name:
+                request_id = '.'.join((unit.relation.relation_id,
+                                       unit.unit_name,
+                                       common_name))
+                req = self.requests[request_id] = CertificateRequest(
+                    request_id=request_id,
+                    cert_type='server',
+                    cert_name=unit.received_raw['certificate_name'],
+                    common_name=common_name,
+                    sans=unit.received['sans'],
+                    unit=unit,
+                    protocol=weakref.proxy(self),
+                )
+                # patch in to req for easier filtering later
+                req._is_top_level_server_cert_request = True
+
+            # handle mutli server cert requests
+            reqs = unit.received['cert_requests'] or {}
+            for common_name, req in reqs.items():
+                request_id = '.'.join((unit.relation.relation_id,
+                                       unit.unit_name,
+                                       common_name))
+                req = self.requests[request_id] = CertificateRequest(
+                    request_id=request_id,
+                    cert_type='server',
+                    cert_name=common_name,
+                    common_name=common_name,
+                    sans=req['sans'],
+                    unit=unit,
+                    protocol=weakref.proxy(self),
+                )
+                # patch in to req for easier filtering later
+                req._is_top_level_server_cert_request = False
+
+            # handle client cert requests
+            reqs = unit.received['client_cert_requests'] or {}
+            for common_name, req in reqs.items():
+                request_id = '.'.join((unit.relation.relation_id,
+                                       unit.unit_name,
+                                       common_name))
+                req = self.requests[request_id] = CertificateRequest(
+                    request_id=request_id,
+                    cert_type='client',
+                    cert_name=common_name,
+                    common_name=common_name,
+                    sans=req['sans'],
+                    unit=unit,
+                    protocol=weakref.proxy(self),
+                )
+                # patch in to req for easier filtering later
+                req._is_top_level_server_cert_request = False
+
+    def _read_responses(self):
+        for request in self.requests.values():
+            rel = request.relation
+            unit_name = request.unit.unit_name.replace('/', '_')
+            # handle top-level server cert requests
+            if request._is_top_level_server_cert_request:
+                cert = rel.to_publish_raw['{}.server.cert'.format(unit_name)]
+                key = rel.to_publish_raw['{}.server.key'.format(unit_name)]
+                if cert and key:
+                    self.responses[request.request_id] = Certificate(
+                        request_id=request.request_id,
+                        cert_type=request.cert_type,
+                        common_name=request.common_name,
+                        cert=cert,
+                        key=key,
+                    )
+            else:
+                if request.cert_type == 'server':
+                    publish_key = '{}.processed_requests'
+                elif request.cert_type == 'client':
+                    publish_key = '{}.processed_client_requests'
+                else:
+                    raise ValueError('Unknown cert_type: '
+                                     '{}'.format(request.cert_type))
+                data = rel.to_publish.get(publish_key.format(unit_name), {})
+                if request.common_name in data:
+                    self.responses[request.request_id] = Certificate(
+                        request_id=request.request_id,
+                        cert_type=request.cert_type,
+                        common_name=request.common_name,
+                        cert=data['cert'],
+                        key=data['key'],
+                    )
+
+    def set_root_ca_cert(self, cert):
+        for relation in self.endpoint.relations:
+            # All the clients get the same CA, so send it to them.
+            relation.to_publish_raw['ca'] = cert
+
+    def set_root_ca_chain(self, chain):
+        for relation in self.endpoint.relations:
+            # All the clients get the same chain, so send it to them.
+            relation.to_publish_raw['chain'] = chain
+
+    def set_global_client_cert(self, cert, key):
+        for relation in self.endpoint.relations:
+            relation.to_publish_raw.update({
+                'client.cert': cert,
+                'client.key': key,
+            })
+
+    def set_cert(self, request, cert, key):
+        rel = request.relation
+        unit_name = request.unit.unit_name.replace('/', '_')
+        if request._is_top_level_server_cert_request:
+            # backwards compatibility; if this is the cert that was requested
+            # as a single server cert, set it in the response as the single
+            # server cert
+            rel.to_publish_raw.update({
+                '{}.server.cert'.format(unit_name): cert,
+                '{}.server.key'.format(unit_name): key,
+            })
+        else:
+            if request.cert_type == 'server':
+                publish_key = '{}.processed_requests'
+            elif request.cert_type == 'client':
+                publish_key = '{}.processed_client_requests'
+            else:
+                raise ValueError('Unknown cert_type: '
+                                 '{}'.format(request.cert_type))
+            data = rel.to_publish.get(publish_key.format(unit_name), {})
+            data[request.common_name] = {
+                'cert': cert,
+                'key': key,
+            }
+            # have to explicit store to ensure serialized data is updated
+            rel.to_publish[publish_key] = data
+        # add this cert to the cache of responses
+        self.responses[request.request_id] = Certificate(
+            request_id=request.request_id,
+            cert_type=request.cert_type,
+            common_name=request.common_name,
+            cert=cert,
+            key=key,
+        )
+
+    def clear(self):
+        rel = self.relation
+        rel.to_publish_raw.update({
+            'ca': None,
+            'chain': None,
+            'client.cert': None,
+            'client.key': None,
+        })
+        for unit in rel.joined_units:
+            unit_name = unit.unit_name.replace('/', '_')
+            rel.to_publish_raw.update({
+                '{}.server.cert'.format(unit_name): None,
+                '{}.server.key'.format(unit_name): None,
+            })
+            rel.to_publish.update({
+                '{}.processed_requests'.format(unit_name): None,
+                '{}.processed_client_requests'.format(unit_name): None,
+            })
--- tls_certificates_common.py
+++ tls_certificates_common.py
@@ -1,36 +1,52 @@
-from charms.reactive import clear_flag, is_data_changed, data_changed
+from charms.reactive import is_data_changed, data_changed, clear_flag
 
 
 class CertificateRequest(dict):
-    def __init__(self, unit, cert_type, cert_name, common_name, sans):
+    def __init__(self, request_id, cert_type, cert_name, common_name, sans,
+                 unit, protocol):
         self._unit = unit
-        self._cert_type = cert_type
+        self._protocol = protocol
         super().__init__({
-            'certificate_name': cert_name,
+            'request_id': request_id,
+            'cert_type': cert_type,
+            'cert_name': cert_name,
             'common_name': common_name,
             'sans': sans,
         })
 
-    @property
-    def _key(self):
-        return '.'.join((self._unit.relation.relation_id,
-                         self.unit_name,
-                         self.common_name))
+    def __missing__(self, key):
+        if key == 'certificate_name':
+            return self.cert_name
+        else:
+            raise KeyError(key)
 
     @property
-    def unit_name(self):
-        return self._unit.unit_name
+    def unit(self):
+        return self._unit
+
+    @property
+    def relation(self):
+        return self.unit.relation
+
+    @property
+    def request_id(self):
+        return self['request_id']
 
     @property
     def cert_type(self):
         """
         Type of certificate, 'server' or 'client', being requested.
         """
-        return self._cert_type
+        return self['cert_type']
 
     @property
     def cert_name(self):
-        return self['certificate_name']
+        """
+        Deprecated.  An optional name used to identify the certificate.
+
+        Use `request_id` instead to unambiguously identify the certificate.
+        """
+        return self['cert_name']
 
     @property
     def common_name(self):
@@ -41,79 +57,37 @@ class CertificateRequest(dict):
         return self['sans']
 
     @property
-    def _publish_key(self):
-        unit_name = self._unit.unit_name.replace('/', '_')
-        if self.cert_type == 'server':
-            return '{}.processed_requests'.format(unit_name)
-        elif self.cert_type == 'client':
-            return '{}.processed_client_requests'.format(unit_name)
-        raise ValueError('Unknown cert_type: {}'.format(self.cert_type))
-
-    @property
-    def _server_cert_key(self):
-        unit_name = self._unit.unit_name.replace('/', '_')
-        return '{}.server.cert'.format(unit_name)
-
-    @property
-    def _server_key_key(self):
-        unit_name = self._unit.unit_name.replace('/', '_')
-        return '{}.server.key'.format(unit_name)
-
-    @property
-    def _is_top_level_server_cert(self):
-        return (self.cert_type == 'server' and
-                self.common_name == self._unit.received_raw['common_name'])
-
-    @property
     def cert(self):
         """
         The cert published for this request, if any.
         """
-        cert, key = None, None
-        if self._is_top_level_server_cert:
-            tpr = self._unit.relation.to_publish_raw
-            cert = tpr[self._server_cert_key]
-            key = tpr[self._server_key_key]
-        else:
-            tp = self._unit.relation.to_publish
-            certs_data = tp.get(self._publish_key, {})
-            cert_data = certs_data.get(self.common_name, {})
-            cert = cert_data.get('cert')
-            key = cert_data.get('key')
-        if cert and key:
-            return Certificate(self.cert_type, self.common_name, cert, key)
-        return None
+        return self._protocol.responses.get(self.request_id)
 
     @property
     def is_handled(self):
         has_cert = self.cert is not None
-        same_sans = not is_data_changed(self._key, self.sans)
+        same_sans = not is_data_changed(self.request_id, self.sans)
         return has_cert and same_sans
 
     def set_cert(self, cert, key):
-        rel = self._unit.relation
-        if self._is_top_level_server_cert:
-            # backwards compatibility; if this is the cert that was requested
-            # as a single server cert, set it in the response as the single
-            # server cert
-            rel.to_publish_raw.update({
-                self._server_cert_key: cert,
-                self._server_key_key: key,
-            })
-        else:
-            data = rel.to_publish.get(self._publish_key, {})
-            data[self.common_name] = {
-                'cert': cert,
-                'key': key,
-            }
-            rel.to_publish[self._publish_key] = data
-        if not rel.endpoint.new_server_requests:
-            clear_flag(rel.endpoint.expand_name('{endpoint_name}.server'
-                                                '.cert.requested'))
-        if not rel.endpoint.new_requests:
-            clear_flag(rel.endpoint.expand_name('{endpoint_name}.'
-                                                'certs.requested'))
-        data_changed(self._key, self.sans)
+        self._protocol.set_cert(self, cert, key)
+        data_changed(self.request_id, self.sans)
+        # update the endpoint's flags to reflect our change in state
+        if not self.relation.endpoint.new_server_requests:
+            clear_flag(self.relation.endpoint.expand_name(
+                '{endpoint_name}.server.certs.requested'))
+            # deprecated legacy flag
+            clear_flag(self.relation.endpoint.expand_name(
+                '{endpoint_name}.server.cert.requested'))
+        if not self.relation.endpoint.new_client_requests:
+            clear_flag(self.relation.endpoint.expand_name(
+                '{endpoint_name}.client.certs.requested'))
+            # deprecated legacy flag
+            clear_flag(self.relation.endpoint.expand_name(
+                '{endpoint_name}.client.cert.requested'))
+        if not self.relation.endpoint.new_requests:
+            clear_flag(self.relation.endpoint.expand_name(
+                '{endpoint_name}.certs.requested'))
 
 
 class Certificate(dict):
@@ -123,13 +97,18 @@ class Certificate(dict):
     The ``cert_type``, ``common_name``, ``cert``, and ``key`` values can
     be accessed either as properties or as the contents of the dict.
     """
-    def __init__(self, cert_type, common_name, cert, key):
+    def __init__(self, request_id, cert_type, common_name, cert, key):
         super().__init__({
+            'request_id': request_id,
             'cert_type': cert_type,
             'common_name': common_name,
             'cert': cert,
             'key': key,
         })
+
+    @property
+    def request_id(self):
+        return self['request_id']
 
     @property
     def cert_type(self):
@@ -146,3 +125,66 @@ class Certificate(dict):
     @property
     def key(self):
         return self['key']
+
+
+class VersionedProtocol:
+    VERSION = None
+    """
+    Integer version number for this implementation.
+
+    Must be set by subclasses.
+    """
+
+    @classmethod
+    def negotiate(cls, relation):
+        """
+        Given a relation instance, return the correct implementation for
+        the maximum mutually supported version.
+
+        If the relation is currently using an older protocol version than
+        is mutually supported, this will call `upgrade_protocol` on the
+        new version, followed by `clear` on the old version.
+        """
+        subclasses = {sub.VERSION: sub for sub in cls.__subclasses__()}
+        if None in subclasses:
+            raise ValueError('Protocol implementation missing version number: '
+                             '{}'.format(subclasses[None].__name__))
+        local_min_version = min(subclasses.keys())
+        local_max_version = max(subclasses.keys())
+        remote_max_version = (relation.joined_units.received['max-version'] or
+                              local_min_version)
+        current_version = (relation.to_publish['current-version'] or
+                           local_min_version)
+        new_version = min(local_max_version, remote_max_version)
+        relation.to_publish['max-version'] = local_max_version
+        if current_version != new_version:
+            old_protocol = subclasses[current_version](relation)
+            new_protocol = subclasses[new_version](relation)
+            new_protocol.upgrade_from(old_protocol)
+            old_protocol.clear()
+        return subclasses[new_version](relation)
+
+    def __init__(self, relation):
+        self.relation = relation
+
+    @property
+    def endpoint(self):
+        return self.relation.endpoint
+
+    def upgrade_protocol(self, old_protocol):
+        """
+        Upgrade to this protocol version from a previous one.
+
+        Must be implemented by subclasses.
+        """
+        raise NotImplementedError()
+
+    def clear(self):
+        """
+        Clear all data in this protocol version's format from the relation.
+
+        Called automatically once the protocol has been upgraded.
+
+        Should be implemented by subclasses.
+        """
+        pass
